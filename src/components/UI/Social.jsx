39f9a7b33a0818f9fe65df6cc76094fc2f75f2b1
Boris Povolotsky <boristofu@gmail.com>
2018-09-08 20:42:24 +0300

    react-icons && about part

--- src/components/UI/Social.jsx
+++ src/components/UI/Social.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FaFacebookF, FaLinkedinIn, FaGithubAlt } from 'react-icons/fa'
 export default () => {
   return (
     <div className="social">
@@ -8,6 +8,23 @@ export default () => {
       <p className="social__about">
         I'm a junior web developer, located in Israel. Currently studying as
         much as I can absorb and looking for a job.
+        <ul className="social__list">
+          <li>
+            <a href="http://facebook.com/povolotsky">
+              <FaFacebookF className="social__item" />
+            </a>
+          </li>
+          <li>
+            <a href="http://github.com/borispov">
+              <FaGithubAlt className="social__item" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/boris-povolotsky/">
+              <FaLinkedinIn className="social__item" />
+            </a>
+          </li>
+        </ul>
       </p>
     </div>
   )
